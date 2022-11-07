dfea53fa6aefeace24a1dcec23bdbc031177c1d1
Jose A. Velasco <jvelasc3@uci.edu>
2022-11-07 15:34:21 -0800

    Book objects stored in an array that can take useer's input and store the new book objects into an array

---
+++ script.js
@@ -0,0 +1,14 @@
+let myLibrary = [];
+
+function Book(Title, Author, Pages, Read) {
+  this.Title = Title;
+  this.Author = Author;
+  this.Pages = Pages;
+  this.Read = Read;
+}
+
+function addBookToLibrary(Title, Author, Pages, Read) {
+  let book = new Book(Title, Author, Pages, Read);
+  myLibrary.push(book);
+  displayBookOnPage();
+}
